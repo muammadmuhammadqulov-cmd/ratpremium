919f9266042e98bb41cd2961879d49e52a128698
muammadmuhammadqulov-cmd <muammadmuhammadqulov@gmail.com>
2026-09-07 10:01:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = '6249830644'
+const id = '8585732036:AAHvXyRuWWPlCNmUNwSCHXAy5aV3UNT_ox4'
 const address = 'https://www.google.com'
 
 const app = express();
